841d99720dfd451bf7ef6fe0e0a3a592a9fcdda3
Katie Owen <katherinehowen@gmail.com>
2016-02-09 09:58:51 -0500

    Created basic user model routes.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+//Dashboard
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users/index');
+});
+
+//Edit Profile
+router.get('/:id/edit', function(req, res, next) {
+  res.render('users/edit');
+});
+
+//Update Profile
+router.put('/:id', function(req, res, next) {
+  var user = User.id(req.params.id);
+  if(!user) return next(makeError(res, 'Document not found', 404));
+  else {
+    user.save()
+    .then(function(saved) {
+      res.redirect('/');
+    }, function(err) {
+      return next(err);
+    });
+  }
+});
+
+//Delete Profile
+router.delete('/:id', function(req, res, next) {
+  var user = User.id(req.params.id);
+  if(!user) return next(makeError(res, 'Document not found', 404));
+  else {
+    user.delete()
+    .then(function(saved) {
+      res.redirect('/');
+    }, function(err) {
+      return next(err);
+    });
 });
 
 module.exports = router;
