00b4f02f13bd133e1546a2c7749437950ff11726
Katie Owen <katherinehowen@gmail.com>
2016-02-09 09:46:36 -0500

    Changes to routes/recipes.js

---
+++ routes/recipes.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+var Recipe = require('../models/recipe');
+var mongoose = require('mongoose');
+
+function makeError(res, message, status) {
+  res.statusCode = status;
+  var error = new Error(message);
+  error.status = status;
+  return error;
+}
+
+var authenticate = function(req, res, next) {
+  if(!req.isAuthenticated()) {
+    res.redirect('/');
+  }
+  else {
+    next();
+  }
+}
+
+//INDEX
+router.get('/', function(req, res, next) {
+  res.render('recipes/index');
+});
+
+//NEW
+router.get('/new', function(req, res, next) {
+  var recipe = {
+    food: '',
+    snack: '',
+    cookingMethod: ''
+  };
+  res.render('recipes/new', { recipe: recipe });
+});
+
+//SHOW
+router.get('/:id', function(req, res, next) {
+  var recipe = Recipe.findOne({_id: req.params.id});
+  //if(!Recipe) return next(makeError(res, 'Document not found', 404));
+  res.render('recipes/show', { recipe: recipe });
+});
+
+//CREATE
+router.post('/', function(req, res, next) {
+  var recipe = new Recipe({
+    food: req.body.food,
+    snack: req.body.snack,
+    cookingMethod: req.body.cookingMethod
+  });
+
+  console.log(recipe);
+  recipe.save()
+  .then(function() {
+    res.redirect('/recipes');
+    next();
+  }, function(err) {
+    return next(err);
+  });
+});
+
+
+
+module.exports = router;
