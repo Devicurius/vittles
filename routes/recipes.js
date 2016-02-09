494c8316e3ad9b07ce89e6585d68b65ba22a4f14
Katie Owen <katherinehowen@gmail.com>
2016-02-08 19:28:58 -0500

    Created base routes for new, create, index for recipes.

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,10 +1,54 @@
 var express = require('express');
 var router = express.Router();
-var recipe = require('../models/recipe');
+var Recipe = require('../models/recipe');
 
-/* GET users listing. */
+function makeError(res, message, status) {
+  res.statusCode = status;
+  var error = new Error(message);
+  error.status = status;
+  return error;
+}
+
+var authenticate = function(req, res, next) {
+  if(!req.isAuthenticated()) {
+    res.redirect('/');
+  }
+  else {
+    next();
+  }
+}
+
+//INDEX
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('recipes/index', { recipes: recipes });
 });
+
+//NEW
+router.get('/new', function(req, res, next) {
+  var recipe = {
+    food: '',
+    snack: '',
+    cookingMethod: ''
+  };
+  res.render('recipes/new', { recipe: recipe });
+});
+
+//CREATE
+router.post('/', function(req, res, next) {
+  var recipe = {
+    food: req.body.food,
+    snack: req.body.snack,
+    cookingMethod: req.body.cookingMethod
+  };
+
+  console.log(recipe)
+  .then(function() {
+    res.redirect('/recipes');
+  }, function(err) {
+    return next(err);
+  });
+});
+
+
 
 module.exports = router;
